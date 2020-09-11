f64dad151e171133a8ea7b6413ff5ad93e81b905
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-09-11 21:33:49 +0300

    Add files via upload

---
+++ src/main/Application.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Application {
+
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		System.out.println("Application RUNNING!!!");
+		A a = new A();
+		B b = new B();
+		C c = null;
+	}
+
+}
+
+class A{}
+class B{}
+class C{}
